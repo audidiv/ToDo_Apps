94764a8f5533b7c66a2214546e30bb30933ace2a
[audi DF] <[audidivaf@gmail.com]>
2023-05-10 14:28:46 +0700

    Feat:GetTasks

--- Controllers/Task.js
+++ Controllers/Task.js
@@ -1,4 +1,5 @@
 import Task from '../model/Task.js';
+import dayjs from 'dayjs';
 
 export const CreateTask = async(req, res, next) => {
     try {
@@ -8,7 +9,7 @@ export const CreateTask = async(req, res, next) => {
         const saveTask = await task.save();
         return res.status(201).json({task:saveTask})
     } catch (error) {
-        next(err);
+        next(error);
     }
 }
 export const UpdateTask = async(req, res, next) => {
@@ -17,20 +18,49 @@ export const UpdateTask = async(req, res, next) => {
         const task = await Task.findByIdAndUpdate(id, {...req.body}, {new: true});
         return res.status(201).json({task});
     } catch (error) {
-        next(err);
+        next(error);
     }
 }
 export const getTask = async(req, res, next) => {
     try {
-        
+        const {id} = req.params;
+        const task = await Task.findById(id);
+        return res.status(201).json({task});
     } catch (error) {
-        next(err);
+        next(error);
     }
 }
 export const getTasks = async(req, res, next) => {
     try {
-        
+        const type = req.query?.type;
+        const day = req.query?.day;
+        const {id} = req.user;
+        var min,max;
+        if(day === 'today'){
+            min = dayjs().format('YYYY-MM-DD');
+            max = dayjs().format('YYYY-MM-DD');
+        } else if (day === 'seven'){
+            min = dayjs().subtract(7, 'day').format('YYYY-MM-DD');
+            max = dayjs().format('YYYY-MM-DD');
+        } else if(day === 'thirty'){
+            min = dayjs().subtract(30, 'day').format('YYYY-MM-DD');
+            max = dayjs().format('YYYY-MM-DD');
+        }
+        if(type){
+            var tasks = await Task.find({
+                userId: id, 
+                type, 
+                ...(day && {
+                    date: {$lte: new Date(max), $gte: new Date(min)}
+                })
+            });
+        } else {
+            var tasks = await Task.find({userId: id, ...(day && {
+                date: {$lte: new Date(max), $gte: new Date(min)}
+            })});
+        }
+        return res.status(201).json({tasks});
     } catch (error) {
-        next(err);
+        next(error);
     }
 }
